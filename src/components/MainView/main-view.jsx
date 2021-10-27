3a34f10f188015c0c82106bc6202cb0fe88fb488
Naom-Roads <nrrodrig@gmail.com>
2021-10-27 17:25:43 +0200

    Made small changes to function name

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-import {LoginView} from '../registration-view/registration-view';
+import {RegistrationView} from '../registration-view/registration-view';
 import {LoginView} from '../login-view/login-view';
 import {MovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
