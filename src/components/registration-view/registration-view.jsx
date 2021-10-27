b68310ca6382e3877703e71cb9aa3a9c893fa64c
Naom-Roads <nrrodrig@gmail.com>
2021-10-27 16:13:56 +0200

    First attempt at Registration Form with hooks, using login-view as template

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,1 +1,34 @@
 import React, { useState } from 'react';
+
+
+export function RegistrationView(props) {
+    const [ email, setEmail] = useState('');
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, username, password);
+        props.onLoggedIn(username);
+    };
+
+    return (
+        <form>
+            <label>
+               Email:
+                <input type="text" value={email} onChange={e => setUsername(e.target.value)}/>
+            </label>
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+}
